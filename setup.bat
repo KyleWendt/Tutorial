88bd1208eb689b6bf065bdaa01410219cbfbb6bd
Kyle Wendt <kylewendt@gmail.com>
2026-04-03 14:08:03 -0700

    init

---
+++ setup.bat
@@ -0,0 +1,12 @@
+start /wait "" Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /RegisterPython=1 /S /D=%UserProfile%Anaconda3
+
+@echo off
+:: 1. Define paths
+set CONDA_PATH=C:\Users\%USERNAME%\anaconda3
+
+:: 2. Initialize Conda for this batch session
+call %CONDA_PATH%\Scripts\activate.bat %CONDA_PATH%
+
+:: 3. Create the environment (if it doesn't exist)
+:: The --yes flag skips the (y/n) confirmation prompt
+call conda create -f conda.yaml
